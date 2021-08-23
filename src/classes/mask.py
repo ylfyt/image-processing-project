2e087d75cd8032ca9a87bfdf69fcaf1d29bd873c
yudialfayat <ayudi3521@gmail.com>
2021-08-23 21:45:48 +0700

    modularitas

--- src/classes/mask.py
+++ src/classes/mask.py
@@ -3,17 +3,19 @@ import numpy as np
 import imutils
 
 class Mask:
-    def __init__(self, name, frame,low, high):
+    def __init__(self, name, low, high):
         self.SIZEOFMINCONTOURAREA = 1000
-        self.THICKNESSOFCONTOUREDGE = 1
+        self.THICKNESSOFCONTOUREDGE = 2
 
         self.name = name
         self.lower = np.array(low)
         self.upper = np.array(high)
-        self.mask = cv2.inRange(src=frame, lowerb=self.lower, upperb=self.upper)
     
     def getMask(self):
         return self.mask
+
+    def updateFrame(self, frame):
+        self.mask = cv2.inRange(src=frame, lowerb=self.lower, upperb=self.upper)
 
     def getContours(self):
         contours = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -22,7 +24,15 @@ class Mask:
 
     def drawContours(self, frame):
         con = self.getContours()
-        for c in con:
+
+        areas = [cv2.contourArea(c) for c in con]
+        idxMax = 0
+        for i in range(1, len(areas)):
+            if (areas[idxMax] < areas[i]):
+                idxMax = i
+
+
+        for idx, c in enumerate(con):
             area = cv2.contourArea(c)
             if (area > self.SIZEOFMINCONTOURAREA):
                 cv2.drawContours(frame, [c], -1, (0, 255, 0), thickness=self.THICKNESSOFCONTOUREDGE)
@@ -32,4 +42,4 @@ class Mask:
                 cy = int(M["m01"]/M["m00"])
 
                 cv2.circle(frame, (cx, cy), 4, (255, 255, 255), -1)
-                cv2.putText(frame, self.name, (cx-0, cy-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+                cv2.putText(frame, self.name, (cx-0, cy-15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), thickness=2)
--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import cv2
 cam = Camera(ipCam=True, width=480, height=320)
 
 def getPicture(frame):
-        cv2.imwrite('img/out.jpg',frame)
-        print("Success")
+    cv2.imwrite('img/out.jpg', frame)
+    print("Success")
 
 def getWaveLength(r, g, b):
     red = int(r)
@@ -14,30 +14,46 @@ def getWaveLength(r, g, b):
     blue = int(b)
     return red * green * blue
 
+def getCondition(wavelength):
+    aaa = 0
+    if (aaa == 1):
+        return "Parah"
+    elif (aaa == 2):
+        return "Tidak parah"
+    else:
+        return "Santuy"
+
+
+redMask = Mask(name="RED", low=[0, 85, 65], high=[10, 255, 255])
+redMask2 = Mask(name="RED", low=[160, 85, 65], high=[179, 255, 255])
+blueMask = Mask(name="BLUE", low=[95, 100, 70], high=[130, 255, 255])
+greenMask = Mask(name="GREEN", low=[35, 95, 70], high=[80, 255, 255])
+orangeMask = Mask(name="ORANGE", low=[10, 100, 20], high=[10, 100, 20])
+exceptWhiteMask = Mask(name="WHITE", low=[0, 42, 0], high=[179, 255, 255])
+
   
 # While loop to continuously fetching data from the Url
 while True:
     # frame = cam.getVideo()
-    frame = cv2.imread("img/out2.jpg")
+    frame = cv2.imread("img/1.jpg")
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    redMask = Mask(name="RED", frame=hsv_frame, low=[0, 85, 65], high=[10, 255, 255])
-    redMask2 = Mask(name="RED", frame=hsv_frame, low=[160, 85, 65], high=[179, 255, 255])
-    blueMask = Mask(name="BLUE", frame=hsv_frame, low=[95, 100, 70], high=[130, 255, 255])
-    greenMask = Mask(name="GREEN", frame=hsv_frame, low=[35, 95, 70], high=[80, 255, 255])
-    orangeMask = Mask(name="ORANGE", frame=hsv_frame, low=[10, 100, 20], high=[10, 100, 20])
-    exceptWhiteMask = Mask(name="WHITE", frame=hsv_frame, low=[0, 42, 0], high=[179, 255, 255])
+    redMask.updateFrame(frame=hsv_frame)
+    redMask2.updateFrame(frame=hsv_frame)
+    blueMask.updateFrame(frame=hsv_frame)
+    greenMask.updateFrame(frame=hsv_frame)
+    orangeMask.updateFrame(frame=hsv_frame)
 
-    # redMask.drawContours(frame=frame)
-    # redMask2.drawContours(frame)
+    redMask.drawContours(frame=frame)
+    redMask2.drawContours(frame)
     blueMask.drawContours(frame)
-    # greenMask.drawContours(frame)
+    greenMask.drawContours(frame)
 
     m = cv2.mean(frame, blueMask.getMask())
     wl = getWaveLength(m[2], m[1], m[0])
 
-    print(m)
+    # print(m)
 
     cv2.imshow("Camera", frame)
 
